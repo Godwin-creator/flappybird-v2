928571a87a11229b899bed0d0606dee43bf620dc
EDOH BEDI Komi Godwin <edohbedigodwin@gmail.com>
2025-12-15 13:48:54 +0000

    Update flappy.js

--- flappy.js
+++ flappy.js
@@ -21,7 +21,7 @@ const largeurTuyau = 40;
 const ecartTuyaux = 80;
 const largeurOiseau = 34;
 const hauteurOiseau = 24;
-const gravite = 1;
+const gravite = 10;
 
 let xoiseau = 100;
 let yoiseau = 150;
